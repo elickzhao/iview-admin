984c04ed0fbad52fb6ba098834a0a09bd46950e2
elickzhao <xwiwi@foxmail.com>
2017-07-03 23:45:12 +0800

    登录表单外观基本完成

---
+++ src/views/login-form.vue
@@ -0,0 +1,55 @@
+<template>
+    <i-form ref="formInline" :model="formInline" :rules="ruleInline">
+        <Form-item>
+            <h1>系统登录</h1>
+        </Form-item>
+        <Form-item prop="user">
+            <Input type="text" placeholder="帐号" size="large">
+            <Icon type="person" size="20" color='#80848f' slot="prepend"></Icon>
+            </Input>
+        </Form-item>
+        <Form-item prop="password">
+            <Input type="password" placeholder="密码" size="large">
+            <Icon type="locked" size="20" color='#80848f' slot="prepend"></Icon>
+            </Input>
+        </Form-item>
+        <Form-item prop="interest">
+            <Checkbox>记住密码</Checkbox>
+        </Form-item>
+        <Form-item>
+            <Button type="primary" @click="handleSubmit('formInline')" long>登录</Button>
+        </Form-item>
+    </i-form>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            formInline: {
+                user: '',
+                password: ''
+            },
+            ruleInline: {
+                user: [
+                    { required: true, message: '请填写用户名', trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: '请填写密码', trigger: 'blur' },
+                    { type: 'string', min: 6, message: '密码长度不能小于6位', trigger: 'blur' }
+                ]
+            }
+        }
+    },
+    methods: {
+        handleSubmit(name) {
+            this.$refs[name].validate((valid) => {
+                if (valid) {
+                    this.$Message.success('提交成功!');
+                } else {
+                    this.$Message.error('表单验证失败!');
+                }
+            })
+        }
+    }
+}
+</script>
--- src/views/login.vue
+++ src/views/login.vue
@@ -2,11 +2,11 @@
     <div class="index">
         <div id="particles-js"></div>
         <div id="text">
-            <h1>Everything Connects</h1>
-            <!--<Button type="ghost" @click="handleStart">Start iView</Button>
-                 <router-link to="/main">
-                <Button type="ghost" @click="handleStart">main</Button>
-                 </router-link>-->
+            <Row>
+                <Col class="login-container">
+                <lFrom></lFrom>
+                </Col>
+            </Row>
         </div>
     </div>
 </template>
@@ -14,7 +14,8 @@
 <script>
 
 import JParticles from 'jparticles';
-import Vue from 'vue';
+//import Vue from 'vue';
+
 
 //引入配置文件
 import particles_config from "../assets/particles1.json";
@@ -40,13 +41,16 @@ export default {
             //这个就更奇葩了.因为是类 不是函数 结果必须在方法里靠点击才能运行 其他的一概不执行 而且他运行也不像文档里写的必须引入particles.js 也够怪的
             new JParticles.particle('#canvas', options);
         }
+    },
+    components:{
+        lFrom:(resolve) => require(['./login-form.vue'], resolve)
     }
 };
 </script>
 
 <style>
 .index {
-    background: #222;
+    background: #f8f8f9;
     margin: 0rem;
     min-height: 100vh;
     /*必须是最小高度  如果是高度的话 下面会有留白 可能是因为和其他高度相对有关*/
@@ -67,16 +71,24 @@ export default {
     min-height: 100vh;
     width: 100vw;
     z-index: 1;
-    color: #fff;
+    /*color: #fff;*/
     text-transform: uppercase;
-    font-size: 8vmin;
+    /*font-size: 5vmin;*/
     display: flex;
     align-items: center;
     justify-content: center;
     text-align: center;
 }
 
-#text h1 {
-    opacity: 0.9
+.login-container{
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+    background-clip: padding-box;
+    margin: 180px auto;
+    width: 450px;                  /*登录form的宽度需要修改这里 */
+    padding: 35px 35px 15px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
 }
 </style>
